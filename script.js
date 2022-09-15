3da8c06eb1444f4c88241abc702a75dbb07dbcb4
Vinícius Ferraz <65097335+VN-rraz@users.noreply.github.com>
2022-09-15 16:45:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
 function calcular(){
-
-var txinicio = document.querySelector('#textoinicio')
-var txfim = document.querySelector('#textofim')
-var txpasso = document.querySelector('#textopasso')
-
+var texto = [document.querySelector('#textoinicio'), document.querySelector('#textofim'), document.querySelector('#textopasso')]
 var res = document.getElementById('res')
-
-    if(txinicio.value.length == 0 || txfim.value.length == 0 || txpasso.value.length == 0){
+    if(texto[0].value.length == 0 || texto[1].value.length == 0 || texto[2].value.length == 0){
         res.innerHTML = 'Impossível contar.'
         alert('[ERRO] Preencha todos os campos.')
     }else{
         res.innerHTML = 'Contando: <br>'
-        var inicio = Number(txinicio.value)
-        var fim = Number(txfim.value)
-        var passo = Number(txpasso.value)
-        if(passo <= 0){
+        var numero = [Number(texto[0].value), Number(texto[1].value), Number(texto[2].value)]
+        if(numero[2] <= 0){
             alert('Passo inválido, considerando passo = 1')
             passo = 1
         }
-
-        if(inicio < fim){
-            for(conta = inicio ;conta <= fim ;conta += passo){
+        if(numero[0] < numero[1]){
+            for(conta = numero[0] ;conta <= numero[1] ;conta += numero[2]){
                 res.innerText += `${conta} \u{270C}`
-      
             }
         }else{
-            for(conta = inicio; conta >= fim;conta -= passo){
+            for(conta = numero[0]; conta >= numero[1];conta -= numero[2]){
                 res.innerHTML += `${conta} \u{270C}`
             }
         }
         res.innerHTML += `\u{270D}`
     }
-
 }
